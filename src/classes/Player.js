692b4e19a3bd6038c79efb0ee16c1fc1d259a451
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-10-14 11:49:32 -0300

    add animation and shoots

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,13 +1,34 @@
+import {
+  INITIAL_FRAMES,
+  PATH_ENGINE_IMAGE,
+  PATH_ENGINE_SPRITES,
+  PATH_SPACESHIP_IMAGE,
+} from "../utils/constants.js";
+import Projectile from "./Projectile.js";
+
 class Player {
   constructor(canvasWidth, canvasHeight) {
-    this.width = 100;
-    this.height = 100;
-    this.speed = 6;
+    this.width = 48 * 2;
+    this.height = 48 * 2;
+    this.speed = 20;
 
     this.position = {
       x: canvasWidth / 2 - this.width / 2,
       y: canvasHeight - this.height - 30,
     };
+
+    this.image = this.getImage(PATH_SPACESHIP_IMAGE);
+    this.engineImage = this.getImage(PATH_ENGINE_IMAGE);
+    this.engineSprites = this.getImage(PATH_ENGINE_SPRITES);
+
+    this.sx = 0;
+    this.framesCounter = INITIAL_FRAMES;
+  }
+
+  getImage(path) {
+    const image = new Image();
+    image.src = path;
+    return image;
   }
 
   moveLeft() {
@@ -19,8 +40,57 @@ class Player {
   }
 
   draw(ctx) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+
+    ctx.drawImage(
+      this.engineSprites,
+      this.sx,
+      0,
+      48,
+      48,
+      this.position.x,
+      this.position.y + 10,
+      this.width,
+      this.height
+    );
+
+    ctx.drawImage(
+      this.engineImage,
+      this.position.x,
+      this.position.y + 8,
+      this.width,
+      this.height
+    );
+
+    this.update();
+  }
+
+  update() {
+    if (this.framesCounter === 0) {
+      this.sx = this.sx === 96 ? 0 : this.sx + 48;
+
+      this.framesCounter = INITIAL_FRAMES;
+    }
+
+    this.framesCounter--;
+  }
+
+  shoot(Projectiles) {
+    const p = new Projectile(
+      {
+        x: this.position.x + this.width / 2 - 1,
+        y: this.position.y + 2,
+      },
+      -10
+    );
+
+    Projectiles.push(p);
   }
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const PATH_SPACESHIP_IMAGE = "./src/assets/images/spaceship.png";
+const PATH_ENGINE_IMAGE = "./src/assets/images/engine.png";
+const PATH_ENGINE_SPRITES = "./src/assets/images/engine_sprites.png";
+const INITIAL_FRAMES = 8;
+
+export {
+  PATH_SPACESHIP_IMAGE,
+  PATH_ENGINE_IMAGE,
+  PATH_ENGINE_SPRITES,
+  INITIAL_FRAMES,
+};
